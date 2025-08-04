9bc12966cc75e22cb3f0df0850e2ac7e21a47493
JeremiasZimmeramn213 <jeremias2205@outlook.com>
2025-08-04 12:25:47 -0400

    feat: enhance application model with ApplicationStatus type and update CreateApplication logic to validate status

--- backend/models/application.go
+++ backend/models/application.go
@@ -2,16 +2,40 @@ package models
 
 import "time"
 
+type ApplicationStatus uint8
+
+const (
+	StatusApplied ApplicationStatus = iota
+	StatusInterview
+	StatusOfferReceived
+	StatusRejected
+)
+
+func (s ApplicationStatus) String() string {
+	switch s {
+	case StatusApplied:
+		return "Applied"
+	case StatusInterview:
+		return "Interview"
+	case StatusRejected:
+		return "Rejected"
+	case StatusOfferReceived:
+		return "Offer Received"
+	default:
+		return "Unknown"
+	}
+}
+
 type Application struct {
-	ID          uint      `gorm:"primaryKey" json:"id"`
-	Company     string    `json:"company"`
-	Position    string    `json:"position"`
-	Status      string    `json:"status"` // e.g., "Applied", "Interview", etc.
-	Location    string    `json:"location"`
-	AppliedDate time.Time `json:"applied_date"`
-	Term        string    `json:"term"`                            // e.g., "Summer 2025"
-	Note        string    `gorm:"size:1048" json:"note,omitempty"` // Optional note, up to 1048 chars
-	ResumeURL   string    `json:"resume_url"`
-	UserID      uint      `json:"user_id"`                                 // Set automatically by server
-	User        User      `gorm:"foreignKey:UserID" json:"user,omitempty"` // Only in responses
+	ID          uint              `gorm:"primaryKey" json:"id"`
+	Company     string            `json:"company"`
+	Position    string            `json:"position"`
+	Status      ApplicationStatus `json:"status"` // e.g., "Applied", "Interview", etc.
+	Location    string            `json:"location"`
+	AppliedDate time.Time         `json:"applied_date"`
+	Term        string            `json:"term"`                            // e.g., "Summer 2025"
+	Note        string            `gorm:"size:1048" json:"note,omitempty"` // Optional note, up to 1048 chars
+	ResumeURL   string            `json:"resume_url"`
+	UserID      uint              `json:"user_id"`                                 // Set automatically by server
+	User        User              `gorm:"foreignKey:UserID" json:"user,omitempty"` // Only in responses
 }
